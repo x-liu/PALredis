4dbdcc78719d9584a3cfc1cca26b5dfe828cd456
Liu <xing.e.liu@accenture.com>
2017-12-14 11:11:44 -0500

    added load balancer using redis

--- start-load-balancer.sh
+++ start-load-balancer.sh
@@ -3,4 +3,4 @@
 [ ! `which nginx` ] && echo "please install nginx - 'brew install nginx'" && exit 1
 
 echo "starting nginx"
-nginx -p support -c nginx.conf
+/C/ProgramData/chocolatey/lib/nginx/tools/nginx-1.12.1/nginx -p support -c nginx.conf
